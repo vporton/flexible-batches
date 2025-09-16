432f8b74c893b57417f1611d112e28c77611dfc1
Victor Porton <porton.victor@gmail.com>
2025-09-16 03:24:37 +0300

    alleged bug fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'], // support both import/export and require()
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.cjs',
+    };
+  },
   dts: true, // generate types
   sourcemap: true,
   clean: true, // clean dist before build
